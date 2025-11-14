05be19d99844a1ac033a8e27f17e0e11c24bd1f8
arifshah985 <arifshahe4u@gmail.com>
2025-11-14 19:46:18 +0500

    custom component profile is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent,ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({  
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrl:'./profile.component.css',
+    standalone: true
+    
+    
+
+})
+
+export class ProfileComponent
+{
+
+}
